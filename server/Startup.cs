e2e2090aded8d2ca968e2df9b619e1368e2b4610
Shaun Russell <shaun.russell@aderant.com>
2019-01-23 13:18:41 +1300

    server displays page and api works

--- server/Startup.cs
+++ server/Startup.cs
@@ -12,34 +12,45 @@ using Microsoft.Extensions.Options;
 
 namespace server
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseDefaultFiles(new DefaultFilesOptions
-            {
-              DefaultFileNames = new
-                List<string> { "index.html" }
-            });
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      // app.UseDefaultFiles(new DefaultFilesOptions
+      // {
+      //   DefaultFileNames = new
+      //     List<string> { "index.html" }
+      // });
+      app.UseDefaultFiles(new DefaultFilesOptions
+      {
+        DefaultFileNames = new
+           List<string> { "index.html" }
+      });
+      app.UseStaticFiles();
+      app.UseMvc();
+      // {
+      //   routes.MapRoute(
+      //     name: "default",
+      //     template: "{controller=Home}/{action=Index}");
+      // });
+    }
+  }
 }
